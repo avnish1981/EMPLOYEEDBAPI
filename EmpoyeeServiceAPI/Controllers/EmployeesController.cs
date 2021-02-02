1697511fe82d2a5cf6d5bbdce7b3bfe20ed18a56
avnish1981 <avnish.choubey@gmail.com>
2021-02-02 17:49:50 +0530

    Adding dataaccess project

---
+++ EmpoyeeServiceAPI/Controllers/EmployeesController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmpDataAccess;
+
+namespace EmpoyeeServiceAPI.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<Employee > Employees()
+        {
+            using (AvnishDBEntities entities = new AvnishDBEntities())
+            {
+                return entities.Employees.ToList();
+            }
+        }
+
+        public Employee Employees(int id)
+        {
+            using (AvnishDBEntities entities = new AvnishDBEntities())
+            {
+                return entities.Employees.FirstOrDefault(e => e.ID == id);
+                
+            }
+        }
+        public HttpResponseMessage InsertEmp([FromBody] Employee employee)
+        {
+            try
+            {
+                using (AvnishDBEntities entities = new AvnishDBEntities())
+                {
+                    entities.Employees.Add(employee);
+                    entities.SaveChanges();
+                }
+                var message = Request.CreateResponse(HttpStatusCode.Created, employee);
+                message.Headers.Location = new Uri(Request.RequestUri + employee.ID.ToString());
+                return message;
+            }
+            catch(Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex.Message);
+            }
+        }
+    }
+}
